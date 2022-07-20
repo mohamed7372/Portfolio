9f20bf58d182c9a56b8586aa4a4378bcf2fe3520
benrabah mohamed <benrabahmohamed530@gmail.com>
2022-07-20 12:24:55 +0100

    init project

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+function NavBar() {
+    return (
+        <div>
+            <p>benrabah</p>
+            <ul>
+                <li>art direction design</li>
+                <li>call me. (+213) 778-013-110</li>
+                <li>/</li>
+                <li>benrabahmohamed530@gmail.com</li>
+            </ul>
+            <img src="" alt="" />
+        </div>
+    );
+}
+
+export default NavBar;
