3eb6236243bf18208f1b712fe6f1cb07df4a96e0
mohamedbenrabah2 <139043715+mohamedbenrabah2@users.noreply.github.com>
2023-07-31 19:31:20 +0100

    add required champ

--- src/components/form/ContactForm.jsx
+++ src/components/form/ContactForm.jsx
@@ -40,18 +40,18 @@ const ContactForm = () => {
                         body={'Thank you for your message.'} />
                 </div>
             }
-            <select name="service" id="" defaultValue={'DEFAULT'} className='rounded-md outline-none text-sm px-2 py-2 bg-secondary-250'>
+            <select name="service" id="" defaultValue={'DEFAULT'} className='rounded-md outline-none text-sm px-2 py-2 bg-secondary-250 placeholder-secondary-200 text-secondary-100'>
                 <option value="DEFAULT" >Choose service you want</option>
                 <option value="website">Need A webSite</option>
                 <option value="deal">Make a deal with you</option>
             </select>
-            <input type="text" name="title" id="title" placeholder='Enter a title' className='placeholder-secondary-200 text-secondary-100 mt-4 rounded-md outline-none px-3 py-2 text-sm bg-secondary-250'/>
+            <input type="text" required name="title" id="title" placeholder='Enter a title' className='placeholder-secondary-200 text-secondary-100 mt-4 rounded-md outline-none px-3 py-2 text-sm bg-secondary-250'/>
 
-            <input type="text" name="name" id="name" placeholder='Enter your name' className='placeholder-secondary-200 text-secondary-100 mt-4 rounded-md outline-none px-3 py-2 text-sm bg-secondary-250'/>
+            <input type="text" required name="name" id="name" placeholder='Enter your name' className='placeholder-secondary-200 text-secondary-100 mt-4 rounded-md outline-none px-3 py-2 text-sm bg-secondary-250'/>
             
-            <input type="email" name="email" id="email" placeholder='Enter your email' className='placeholder-secondary-200 text-secondary-100 mt-4 rounded-md outline-none px-3 py-2 text-sm bg-secondary-250'/>
+            <input type="email" required name="email" id="email" placeholder='Enter your email' className='placeholder-secondary-200 text-secondary-100 mt-4 rounded-md outline-none px-3 py-2 text-sm bg-secondary-250'/>
 
-            <textarea name="message" id="message" placeholder='Enter your message' cols="30" rows="10" className='placeholder-secondary-200 text-secondary-100 mt-4 rounded-md outline-none px-3 py-2 text-sm bg-secondary-250 max-h-64 min-h-[40px]'></textarea>
+            <textarea required name="message" id="message" placeholder='Enter your message' cols="30" rows="10" className='placeholder-secondary-200 text-secondary-100 mt-4 rounded-md outline-none px-3 py-2 text-sm bg-secondary-250 max-h-64 min-h-[40px]'></textarea>
 
             <button type="submit" className='rounded-md border mt-4 border-primary-200 text-primary-200 py-3 px-8 w-fit m-auto hover:bg-primary-200 hover:bg-opacity-20 transition duration-300 ease-in'>Submit</button>
         </form>
